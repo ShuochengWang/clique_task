1128b2025334899be5436f26bae7344d8ebda31b
Shuocheng Wang <wangshch5@outlook.com>
2024-04-24 09:40:26 +0800

    Remove useless files

--- run_neo4j_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it \
-    --name neo4j \
-    --publish=7474:7474 --publish=7687:7687 \
-    --env-file .env \
-    --volume=$HOME/code/clique_task/neo4j/data:/data \
-    --volume=$HOME/code/clique_task/neo4j/log:/log \
-    neo4j
